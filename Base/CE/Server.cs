5bc58de437a4a3d504880cf9f283a1648f51485f
Null 774 <49384723+null774@users.noreply.github.com>
2020-11-02 14:20:04 -0500

    Update Server.cs

--- Base/CE/Server.cs
+++ Base/CE/Server.cs
@@ -10,35 +10,6 @@ namespace Graal_774_Client.Base.CE
     public class Server
     {
 
-        public static ulong Execute(string serverName, string cmdString)
-        {
-            NamedPipeClientStream s = new NamedPipeClientStream(serverName);
-            s.Connect();
-
-            if (s.IsConnected)
-            {
-                byte[] result = new byte[8];
-                byte[] command = Encoding.ASCII.GetBytes(cmdString);
-                byte[] size = BitConverter.GetBytes((int)command.Length);
-
-                byte[] fullcommand = new byte[1];
-
-                fullcommand[0] = 1; // execute string
-                fullcommand = fullcommand.Concat(size).ToArray();
-                fullcommand = fullcommand.Concat(command).ToArray();
-
-                fullcommand = fullcommand.Concat(BitConverter.GetBytes((long)0)).ToArray(); //the 'parameter' value
-
-                s.Write(fullcommand, 0, fullcommand.Length);
-
-                s.Read(result, 0, 8);
-
-                s.Close();
-
-                return BitConverter.ToUInt64(result, 0);
-            }
-            else
-                return 0;
-        }
+       Fuck.You.Skid();
     }
 }
